308ebc271ed25057d484cb2d9d461e839fae9c8c
jssrll <jessrell1010@gmail.com>
2026-03-29 00:46:28 +0800

    Fixed login with phone number format handling

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ let currentPage = "home";
 let currentUser = null;
 
 // Your Google Sheets Web App URL
-const GOOGLE_SHEETS_URL = "https://script.google.com/macros/s/AKfycbwd9WXCZ3e6mMWakTL5d7sauQQAZtKFELiy4BNxFhB0-lAJO1DzCnKyXQ9ecxy8mATKcA/exec";
+const GOOGLE_SHEETS_URL = "https://script.google.com/macros/s/AKfycbzquscqnI7nZbjCsKRhyXFyBQrzPM-SH9ZwV0K2SgLkGW_rvEwAe-TAzGQGjALd8jsdZA/exec";
 
 // ========================================
 // HELPER FUNCTIONS
